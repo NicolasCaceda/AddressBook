d2deab50d4e2645b7cc613601a1309927381c110
Nicolas Caceda <nicolas.caceda@gmail.com>
2020-04-18 20:22:18 -0400

    update gitignore and new unit test Addressbook @95%

--- test/AddressBook/AddressBookControllerTest.java
+++ test/AddressBook/AddressBookControllerTest.java
@@ -3,6 +3,10 @@ package AddressBook;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AddressBookControllerTest {
@@ -20,13 +24,22 @@ class AddressBookControllerTest {
     }
 
     @Test
-    void open() {
+    void nothing_open_exception() {
         assertThrows(NullPointerException.class, () -> {
             testAddressBookController.open(null);
         });
     }
 
+    //depends on save and clear
     @Test
-    void save() {
+    void stuffExists_open_DoesNotThrow() throws SQLException, FileNotFoundException {
+        //setup for opening a good file
+        File testFile = new File("openTest.txt");
+        testAddressBook.add(dummyPerson);
+        testAddressBookController.save(testFile);
+        testAddressBook.clear();
+        //The assertion
+        assertDoesNotThrow(() -> testAddressBookController.open(testFile));
     }
+
 }
